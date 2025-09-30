31c5b036050a143e22a97b185d316e9679aff472
DugkatniIdol <jmmanlapaz5@gmail.com>
2025-09-30 09:26:26 +0200

    quadratoCheCambiaColore

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello");
+function nero() {
+    document.getElementById("quadrato").style.backgroundColor = "black";
+}
+
+function blu() {
+    document.getElementById("quadrato").style.backgroundColor = "blue";
+}
+
+function giallo() {
+    document.getElementById("quadrato").style.backgroundColor = "yellow";
+}
+
+function rosa() {
+    document.getElementById("quadrato").style.backgroundColor = "pink";
+}
